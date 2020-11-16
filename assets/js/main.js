01e0b79ac06d1cc71dbab5abce4ac0456a07a7ae
qi lin <qlin1996@gmail.com>
2020-11-15 19:29:40 -0500

    removed contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,5 +85,3 @@ sr.reveal('.projects__img', { interval: 200 });
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {});
 sr.reveal('.contact__text', { interval: 200 });
-sr.reveal('.contact__input', { delay: 400 });
-sr.reveal('.contact__button', { delay: 600 });
