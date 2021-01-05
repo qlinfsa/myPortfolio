e6b0a72c528ca6dd9d20c48fc68a77dc3f1bfba5
Qi (Q) Lin <58145741+qlin1996@users.noreply.github.com>
2021-01-04 20:12:37 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*===== SHOW MENU =====*/
+/*===== TOGGLE MENU =====*/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
@@ -11,7 +11,7 @@ const showMenu = (toggleId, navId) => {
 };
 showMenu('nav-toggle', 'nav-menu');
 
-/*===== REMOVE MENU MOBILE =====*/
+/*===== CLICK ON LINK AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
